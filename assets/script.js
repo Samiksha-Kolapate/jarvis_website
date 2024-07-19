de2c385caf60decde290c0693865277a423030dd
Samiksha-Kolapate <kolapatesamiksha@gmail.com>
2024-07-19 21:47:23 +0530

    jarvis website

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+ /* partners swiper strated */
+ // Initialize Swiper
+
+ var swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+ 
+    spaceBetween: 30,
+    autoplay:{
+      delay:4000,
+
+    },
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+ /* partners swiper ended */
+
+ /* navbar menu display started */ 
+function showMenu(){
+  const navList = document.querySelectorAll('.nav-list');
+  
+  }
+  
+  /* navbar menu display ended */ 
+   
